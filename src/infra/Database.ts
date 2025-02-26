432d42f84ee587a8f78a7ec30a69baf6472d530f
Yves <yves.as@gmail.com>
2025-02-26 10:45:05 -0300

    fixed database connection

--- src/infra/Database.ts
+++ src/infra/Database.ts
@@ -6,10 +6,13 @@ dotenv.config();
 export class Database {
   public async connect(): Promise<void> {
     try {
-      await mongoose.connect(process.env.DB_CONNECT as string, {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
-      });
+      await mongoose.connect(process.env.DATABASE_URL as string);
+      // {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+      //   useFindAndModify: false,
+      //   useCreateIndex: true,
+      // }
       console.log("✅ Connected to MongoDB Atlas");
     } catch (err) {
       console.error("❌ Error connecting to MongoDB:", err);
